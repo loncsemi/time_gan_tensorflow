0c9d741bac2fae7ec9bd5a1906cab2f78a45c0ba
Michael Loncsek <95223026+lonmistbl@users.noreply.github.com>
2025-03-01 17:12:44 +0100

    updated losses

--- time_gan_tensorflow/losses.py
+++ time_gan_tensorflow/losses.py
@@ -2,19 +2,16 @@ import tensorflow as tf
 
 @tf.function
 def mean_squared_error(y_true, y_pred):
-    '''
+    """
     Mean squared error, used for calculating the supervised loss and the reconstruction loss.
-    '''
-    loss = tf.keras.losses.mean_squared_error(y_true=tf.expand_dims(y_true, axis=-1), y_pred=tf.expand_dims(y_pred, axis=-1))
+    """
+    mse = tf.keras.losses.MeanSquaredError(reduction=tf.keras.losses.Reduction.NONE)
+    loss = mse(y_true, y_pred)
     return tf.reduce_mean(tf.reduce_sum(loss, axis=-1))
-
 
 @tf.function
 def binary_crossentropy(y_true, y_pred):
-    '''
+    """
     Binary cross-entropy, used for calculating the unsupervised loss.
-    '''
-    loss = tf.keras.losses.binary_crossentropy(y_true=y_true, y_pred=y_pred, from_logits=True)
-    return tf.reduce_mean(loss)
-
-
+    """
+    bce = tf.keras.losses.BinaryCrossentropy(from_logits=False, reduction=tf.keras.losses.Reduction.NONE)
